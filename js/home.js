c9126258de471e43261c5013d31adfa1ba2f17ab
HankWang <15768616006@163.com>
2017-11-10 15:29:29 +0800

    webapp

---
+++ js/home.js
@@ -0,0 +1,20 @@
+/*(function($){
+	$.init({
+		gestureConfig:{
+			tap:true,
+			doubletap:true,
+			longtap:true,
+			swipeleft:true,
+			drag:true,
+			hold:false,
+			release:false
+		}
+
+	});
+	var colllegenews = document.getElementById('news');
+
+	collegeNews.addEventListener('tap',function(){
+		alert('ssss');
+	})
+}(window.mui = window.mui || {}))
+*/
